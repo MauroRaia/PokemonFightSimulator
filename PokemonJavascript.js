7e9fb84dcb3a604207d5dafdeae158eab6c30d8a
Alumno <alumno@labapc18.escuela>
2015-11-06 17:24:47 -0300

    logica, un ataque y su daño

--- PokemonJavascript.js
+++ PokemonJavascript.js
@@ -26,18 +26,51 @@ function loadTextFileAjaxSync(filePath, mimeType)
     return null;
   }
 }
+$( document ).ready(function() {
 
-function battleType(jsonAttacker, jsonDefender){
-//  if (jsonAttacker.type)
-}
-function dmgPokemon(jsonAttacker, jsonDefender){
-  var dmgPokemon = (((2*jsonAttacker.level+10)/250)*(jsonAttacker.Attack/jsonDefender.Defense) * (jsonAttacker.attack.dmg + 2));
-  return dmgPokemon;
-}
+  var ataques = loadJSON("Attacks.json");
+  var types = loadJSON("Types.json");
+  var pokemonA = loadJSON("Ivysaur.json");
+  var pokemonB = loadJSON("Scyther.json");
+  $(".vida").html(pokemonB.stats.Hp);
 
-function advantage(typeAttack, typePokemon){
-//  if
-}
+  function modifier(){
+    //a = Pokemon attack
+    //b = Pokemon defending
+    var a = ataques[pokemonA.Attacks[0]];
 
-var ataques = loadJSON("Attacks.json");
-var types = loadJSON("Types.json");
+    if (_.contains(types[pokemonB.types[0]].WeakAgainst, a.Type)){
+      var result = 2;
+    }
+      else if (_.contains(types[pokemonB.types[0]].StrongAgainst, a.Type)) {
+        var result = 0.5;
+      }
+      else {
+        var result = 1;
+      }
+    return result
+  }
+
+  console.log(modifier());
+
+  function hit(){
+    if (ataques[pokemonA.Attacks[0]].Category == "Physical"){
+      var dmgPokemon = ((((2 * pokemonA.level + 10) / 250) * (pokemonA.stats.Attack / pokemonB.stats.Defense) * (ataques[pokemonA.Attacks[0]].Dmg + 2)) * modifier());
+      pokemonB.stats.Hp = Math.round(pokemonB.stats.Hp - dmgPokemon);
+      return pokemonB.stats.Hp;
+      }
+    else {
+      var dmgPokemon = ((((2 * pokemonA.level + 10) / 250) * (pokemonA.stats.SpAtk / pokemonB.stats.SpDef) * (ataques[pokemonA.Attacks[0]].Dmg + 2)) * modifier());
+      pokemonB.stats.Hp = Math.round(pokemonB.stats.Hp - dmgPokemon);
+      return pokemonB.stats.Hp;
+    }
+  }
+
+  $(document).keypress(function(e) {
+      if(e.which == 13) {
+          console.log(hit());
+          $(".vida").html(pokemonB.stats.Hp);
+      }
+  });
+
+});
